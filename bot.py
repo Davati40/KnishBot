b7d08c5636e12af1df3f0132974b60df7b4d31ff
Iain Davati <19866459+Davati40@users.noreply.github.com>
2020-03-05 10:18:39 -0600

    Update bot.py
    Updated code to run with Discord.py version 1.3.2 and Python 3.8.2.

--- bot.py
+++ bot.py
@@ -2,139 +2,132 @@ import discord
 from discord.ext import commands
 import logging
 
-
-description = '''Knish The Dog In Bot Form.
-Please Use All Lowercase Letters.'''
-bot = commands.Bot(command_prefix='>', description=description)
+bot = commands.Bot(command_prefix='>')
 logging.basicConfig(level=logging.INFO)
 
+description = '''Knish The Dog In Bot Form. Please Use All Lowercase Letters.'''
+
+activity = discord.Game(name="nothing. I'm napping.")
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(game=discord.Game(name="over my family | >help", type=3), status=discord.Status("online"))
-	#0=Playing 1=Streaming 2=Listening 3=Watching
-	print('')
-	print('------')
-	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
-	print('------')
-	print('Running API Version: ' + discord.__version__)
-	print('------')
-	print('')
-
+    await bot.change_presence(activity=activity)
+    print('')
+    print('------')
+    print('Logged in as:\n{0} (ID:{0.id})'.format(bot.user))
+    print('------')
+    print('Running API Version:' + discord.__version__)
+    print('------')
+    print('')
 
 
 # General Commands
 
 @bot.command()
-async def hi():
-	'''Say Hi To Me.'''
-	await bot.say('Woof!')
-	
+async def hi(ctx):
+    '''Say Hi To Me.'''
+    await ctx.send('Woof!')
+
 @bot.command()
-async def aww():
-	'''Basicly saying I'm Cute.'''
-	await bot.say('Happy Woof!')
-	
+async def aww(ctx):
+    '''Basicly saying I'm Cute.'''
+    await ctx.send('Happy Woof!')
+
 @bot.command()
-async def bork():
-	'''Says 'bork' To Me.'''
-	await bot.say('*runs around house at top speed*')
-	
+async def bork(ctx):
+    '''Says 'bork' To Me.'''
+    await ctx.send('*runs around house at top speed*')
+
 @bot.command()
-async def quiet():
-	'''Tells Me To Be Quiet.'''
-	await bot.say('Loud Bark!')
-	
+async def quiet(ctx):
+    '''Tells Me To Be Quiet.'''
+    await ctx.send('Loud Bark!')
+
 @bot.command()
-async def sit():
-	'''Tells Me To Sit.'''
-	await bot.say('*lays*')
-	
+async def sit(ctx):
+    '''Tells Me To Sit.'''
+    await ctx.send('*lays*')
+
 @bot.command()
-async def lay():
-	'''Tells Me To Lay Down.'''
-	await bot.say('*sits*')
-	
+async def lay(ctx):
+    '''Tells Me To Lay Down.'''
+    await ctx.send('*sits*')
+
 @bot.command()
-async def squirrel():
-	'''Did Somebody Say Squirrel?!'''
-	await bot.say('OMG SQUIRREL WHAT WHERE')
-	
+async def squirrel(ctx):
+    '''Did Somebody Say Squirrel?!'''
+    await ctx.send('OMG SQUIRREL WHAT WHERE')
+
 @bot.command()
-async def add():
+async def add(ctx):
   '''Add Me To A Server.'''
-  await bot.say('https://discordapp.com/api/oauth2/authorize?client_id=413728059645100039&permissions=36957248&scope=bot')
-  
-  
-  
+  await ctx.send('https://discordapp.com/api/oauth2/authorize?client_id=413728059645100039&permissions=36957248&scope=bot')
+
+
+
 # 'Cool' Command
 
 @bot.group(pass_context=True)
 async def cool(ctx):
-	"""Says If Someone Is Cool."""
-	if ctx.invoked_subcommand is None:
-		await bot.say('Growl..!'.format(ctx))
+    """Says If Someone Is Cool."""
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Growl..!'.format(ctx))
 
 
 @cool.command(name='knish')
-async def _bot():
-	await bot.say('*Happy Bork!*')
-
-
-@cool.command(name='alex')
-async def _bot():
-	await bot.say('*Happy Bork!*')
+async def _bot(ctx):
+    await ctx.send('*Happy Bork!*')
 
 
 @cool.command(name='davati')
-async def _bot():
-	await bot.say('*Happy Bork!*')
+async def _bot(ctx):
+    await ctx.send('*Happy Bork!*')
 
 
 @cool.command(name='whatever you want to say')
-async def _bot():
-	await bot.say('Growl..!')
+async def _bot(ctx):
+    await ctx.send('Growl..!')
 
 
 
 # Photos Below
-	
+
 @bot.command()
-async def sleep():
+async def sleep(ctx):
   '''Shows Me Sleeping.'''
-  await bot.say('https://i.imgur.com/Dwr7SE2.jpg')
-	
+  await ctx.send('https://i.imgur.com/Dwr7SE2.jpg')
+
 @bot.command()
-async def god():
+async def god(ctx):
   '''Shows Me In My True Form.'''
-  await bot.say('https://i.imgur.com/1ALnOwp.jpg')
+  await ctx.send('https://i.imgur.com/1ALnOwp.jpg')
 
 
 @bot.command()
-async def alert():
+async def alert(ctx):
   '''Shows Me Being Alert (and definately not scared).'''
-  await bot.say('https://i.imgur.com/sfmwytD.jpg')
+  await ctx.send('https://i.imgur.com/sfmwytD.jpg')
 
 
 @bot.command()
-async def hello():
-	'''Shows Me Being Friendly.'''
-	await bot.say('https://i.imgur.com/LOrTu7N.jpg')
+async def hello(ctx):
+    '''Shows Me Being Friendly.'''
+    await ctx.send('https://i.imgur.com/LOrTu7N.jpg')
 
 @bot.command()
-async def snow():
-	'''Shows Me Walking Through Snow.'''
-	await bot.say('https://i.imgur.com/xYWit3A.jpg')
-	
+async def snow(ctx):
+    '''Shows Me Walking Through Snow.'''
+    await ctx.send('https://i.imgur.com/xYWit3A.jpg')
+
 @bot.command()
-async def sun():
+async def sun(ctx):
   '''Shows Me Sunbathing.'''
-  await bot.say('https://i.imgur.com/ykjEPtZ.jpg')
+  await ctx.send('https://i.imgur.com/ykjEPtZ.jpg')
 
 @bot.command()
-async def spotted():
+async def spotted(ctx):
   '''I've Been Seen!'''
-  await bot.say('https://i.imgur.com/6UTehGk.jpg')
+  await ctx.send('https://i.imgur.com/6UTehGk.jpg')
 
 
-bot.run('INSERT_TOKEN_HERE')
+bot.run('INSERT_CODE_HERE')
